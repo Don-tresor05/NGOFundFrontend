962836a2b0c3abd0b7a402e347a5c21ab778f455
Don-tresor05 <dontresor922@gmail.com>
2026-03-03 10:38:37 +0200

    changed the modules from coming soon to what  i have done to implement

--- src/pages/modules/index.tsx
+++ src/pages/modules/index.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
 export { DonorManagement } from './DonorManagement';
-
-export const FundTracking: React.FC = () => (
-  <div className="p-8">
-    <h2 className="text-3xl font-bold mb-4">Fund Tracking Module</h2>
-    <p className="text-gray-600">Coming soon...</p>
-  </div>
-);
-
-export const ProjectAllocation: React.FC = () => (
-  <div className="p-8">
-    <h2 className="text-3xl font-bold mb-4">Project Allocation Module</h2>
-    <p className="text-gray-600">Coming soon...</p>
-  </div>
-);
-
-export const ExpenditureMonitoring: React.FC = () => (
-  <div className="p-8">
-    <h2 className="text-3xl font-bold mb-4">Expenditure Monitoring Module</h2>
-    <p className="text-gray-600">Coming soon...</p>
-  </div>
-);
+export { FundTracking } from './FundTracking';
+export { ProjectAllocation } from './ProjectAllocation';
+export { ExpenditureMonitoring } from './ExpenditureMonitoring';
 
 export const StaffOperations: React.FC = () => (
   <div className="p-8">
