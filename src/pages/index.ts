315bf58e9f82a971f5f02d9edafd94bf0e38b54d
Don-tresor05 <dontresor922@gmail.com>
2026-06-17 22:45:05 +0200

    Export Module 8, 9, and 10 pages from index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -9,3 +9,14 @@ export { ConfirmPasswordPage } from './ConfirmPasswordPage';
 export { ResetPasswordPage } from './ResetPasswordPage';
 export { UseCasePage } from './UseCasePage';
 export { PublicProjectsPage } from './PublicProjectsPage';
+
+// Super Admin Pages
+export { default as SuperAdminUserManagementPage } from './SuperAdminUserManagementPage';
+export { default as PermissionMatrixEditorPage } from './PermissionMatrixEditorPage';
+export { default as AuditTrailExplorerPage } from './AuditTrailExplorerPage';
+export { default as LoginActivityViewerPage } from './LoginActivityViewerPage';
+export { default as SessionSettingsPage } from './SessionSettingsPage';
+export { default as CustomizableDashboardPage } from './CustomizableDashboardPage';
+export { default as ComplianceChecklistPage } from './ComplianceChecklistPage';
+export { default as DocumentRepositoryPage } from './DocumentRepositoryPage';
+export { default as ExceptionReportPage } from './ExceptionReportPage';
